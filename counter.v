c86011f8b0f33b60836671c433b01ab18d3515c7
cpmori <cpmori@ncsu.edu>
2023-01-17 19:57:32 -0500

    moved counter module to own file; refitting top module with handlers and coutner

---
+++ counter.v
@@ -0,0 +1,30 @@
+module counter(
+    input counter_sel,
+    input reset_b,
+    input clk,
+    output wire[19:0] counter_value
+);
+
+reg[4:0] counter_reg;
+
+always@(posedge clk or negedge reset_b)
+begin
+    if(!reset_b) counter_reg <= 20'b0;
+    else
+        case(counter_sel)
+            2'b00:
+                counter_reg = 20'b0;
+            2'b01:
+                counter_reg = counter_reg;
+            2'b10:
+                counter_reg = counter_reg;
+            2'b11:
+                counter_reg = counter_reg + 1;
+        endcase
+end
+
+assign counter_value = counter_reg;
+
+
+endmodule
+
