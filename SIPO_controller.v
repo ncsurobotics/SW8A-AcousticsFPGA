96aa0acd2b453c9bf7557f7144cd7a54dc5b9678
cpmori <cpmori@ncsu.edu>
2023-01-17 20:16:46 -0500

    removed unused parameters describes in SIPO controller

--- SIPO_controller.v
+++ SIPO_controller.v
@@ -13,18 +13,8 @@ module SIPO_controller(
                 S1       =      4'd1,
                 S2       =      4'd2,
                 S3       =      4'd3,
-                S4       =      4'd4,
-                S5       =      4'd5,
-                S6       =      4'd6,
-                S7       =      4'd7,
-                S8       =      4'd8,
-                S9       =      4'd9,
-                S10      =      4'd10,
-                S11      =      4'd11,
-                S12      =      4'd12,
-                S13      =      4'd13,
-                S14      =      4'd14,
-                S15      =      4'd15;
+                S4       =      4'd4;
+
 
     reg [3:0] current_state, next_state;
 
@@ -95,35 +85,5 @@ endmodule
 
 
 
-
-module counter(
-    input counter_sel,
-    input reset_b,
-    input clk,
-    output wire[19:0] counter_value
-);
-
-reg[4:0] counter_reg;
-
-always@(posedge clk or negedge reset_b)
-begin
-    if(!reset_b) counter_reg <= 20'b0;
-    else
-        case(counter_sel)
-            2'b00:
-                counter_reg = 20'b0;
-            2'b01:
-                counter_reg = counter_reg;
-            2'b10:
-                counter_reg = counter_reg;
-            2'b11:
-                counter_reg = counter_reg + 1;
-        endcase
-end
-
-assign counter_value = counter_reg;
-
-
-endmodule
 
 
