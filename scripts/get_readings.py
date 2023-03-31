a72428fe591dda12207b588f213b1cb93fca8e10
imjohns3 <imjohns3@ncsu.edu>
2023-03-31 08:01:06 -0400

    update get_readings.py

--- scripts/get_readings.py
+++ scripts/get_readings.py
@@ -1,20 +1,33 @@
 import serial
+import readline
 
 ser = serial.Serial('COM6', 115200, timeout = 1) # open serial port
 print(ser.name)
 
-# Send 'A' to the FPGA to tell it to give you data
-get_data = b'\x41'
-ser.write(get_data)
+while True:
 
-# each channel reading is a 10 bit number. the FPGA sends a channel
-# reading in 3 bytes:
-#   1st byte: channel ID: 'A', 'B', 'C', or 'D' -- right now only 'A'
-#   2nd byte: 3'b100 + 5 LSB
-#   3rd byte: 3'b110 + 5 MSB
+    # Send 'A' to the FPGA to tell it to give you data
+    get_data = b'\x41'
+    ser.write(get_data)
 
-# read 1 channel, so 3 bytes
-data = ser.read(3)
+    # each channel reading is a 10 bit number. the FPGA sends a channel
+    # reading in 3 bytes:
+    #   1st byte: channel ID: 'A', 'B', 'C', or 'D' -- right now only 'A'
+    #   2nd byte: 3'b100 + 5 LSB
+    #   3rd byte: 3'b100 + 5 MSB
 
-print("got: ") 
-print(data)
+    # read 1 channel, so 3 bytes
+    data = ser.read(3)
+
+    # convert to voltage
+    data_list = list(data)
+    data_10bit = ((data_list[2] & 0b00011111) << 5) | (data_list[1] & 0b00011111)
+    data_int = int(data_10bit)
+
+    volts = (data_int * 5.0) / 1023.0
+
+    print("got raw: " + str(data) + ", voltage = " + str(volts)) 
+
+    run_again = input("run again? (y/n)")
+    if run_again in ("n", "N"):
+        break
