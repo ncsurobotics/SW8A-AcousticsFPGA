117640ad978e3837dfd7571c64dced86dcac431a
imjohns3 <imjohns3@ncsu.edu>
2023-07-01 15:46:36 -0400

    pool test script functional to request and receive data

--- scripts/get_readings3.py
+++ scripts/get_readings3.py
@@ -6,7 +6,7 @@ import keyboard # pip install keyboard
 
 #port = input("enter port name: ")
 
-ser = serial.Serial('COM6', 115200, timeout = 1) # open serial port
+ser = serial.Serial('COM4', 115200, timeout = 1) # open serial port
 print(ser.name)
 
 start_time = time.time()
@@ -17,31 +17,31 @@ instruction = b'x41'
 
 while True:
 
+    check_input = input()
+    if check_input in ("stop"):
+        break
+    elif (check_input in ("1")):
+        instruction = b'A'
+    elif (check_input in ("2")):
+        instruction = b'B'
+    elif (check_input in ("3")):
+        instruction = b'C'
+    elif (check_input in ("4")):
+        instruction = b'D'
+
     # Send 'A' to the FPGA to tell it to give you data
     ser.write(instruction)
 
     # read 1 channel (2 bytes)
-    data = ser.read(2)
+    data = ser.read(1)
 
     # convert to voltage
-    data_list = list(data)
-    data_16bit = data_list[1] + data_list[0]
-    data_int = int(data_16bit)
+    data_8b = int.from_bytes(data, 'big')
+    data_int = data_8b << 2
 
     volts = (data_int * 5.0) / 1023.0
 
     print("[{}] ".format(int((time.time() - start_time) * 1000)) + "got raw: " + str(data) + ", voltage = " + str(volts)) 
-
-    check_input = input()
-    if check_input in ("stop"):
-        break
-    else if check_input in ("1"):
-        instruction = b'x41'
-    else if check_input in ("2"):
-        instruction = b'x42'
-    else if check_input in ("3"):
-        instruction = b'x43'
-    else if check_input in ("4"):
-        instruction = b'x44'
     
     time.sleep(0.1)
+    
