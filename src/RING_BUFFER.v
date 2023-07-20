ca333577f8d0e17ed3ba2cf4ec39d61f12c6f215
cpmori <cpmori@ncsu.edu>
2023-07-19 20:24:44 -0400

    Added files for FFT triggering

---
+++ src/RING_BUFFER.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/19/2023 08:18:36 PM
+// Design Name: 
+// Module Name: RING_BUFFER
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RING_BUFFER(
+
+    input [9:0] Input_Data,
+    
+    
+    output RAM_Overflow,
+
+);
+endmodule
---
+++ src/TRIGGER_FFT.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/19/2023 08:18:36 PM
+// Design Name: 
+// Module Name: TRIGGER_FFT
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module TRIGGER_FFT(
+
+    );
+endmodule
