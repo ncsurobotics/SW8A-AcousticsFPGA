3097467b064381a1f413cc34b1583fdb28ef8285
imjohns3 <imjohns3@ncsu.edu>
2023-07-21 00:40:02 -0400

    fix TRIGGER_FFT.v, add TRIGGER_FFT_tb.v

--- src/FFT_CONTROLLER.v
+++ src/FFT_CONTROLLER.v
@@ -1,5 +1,8 @@
 module FFT_CONTROLLER ( input clk,
                         input reset_b,
+                        
+                        input data_ready, // for fourth_sample_counter_sel
+                        input fourth_sample_reached,
 
                         input s_axis_config_tready,     // from FFT
                         input enable,
@@ -10,6 +13,7 @@ module FFT_CONTROLLER ( input clk,
                         output reg s_axis_data_tlast,
 
                         output reg [1:0] samples_sent_counter_sel, // outputs to TRIGGER_FFT block
+                        output [1:0] fourth_sample_counter_sel,
 
                         //output reg start_frame,             // outputs to RING_BUFFER
                         //output reg end_frame,
@@ -39,91 +43,161 @@ end
 always @ (*) begin
     case (current_state)
         START: begin                    // wait for config_tready to go high to config
-            s_axis_config_tvalid     = 1'b0;
-            s_axis_data_tvalid       = 1'b0;
-            s_axis_data_tlast        = 1'b0;
-            samples_sent_counter_sel = ZERO;
+            s_axis_config_tvalid     <= 1'b0;
+            s_axis_data_tvalid       <= 1'b0;
+            s_axis_data_tlast        <= 1'b0;
+            samples_sent_counter_sel <= ZERO;
             //start_frame              = 1'b0;
             //end_frame                = 1'b0;
-            send_frame               = 1'b0;
+            send_frame               <= 1'b0;
 
-            if (s_axis_config_tready) next_state = CONFIG_FFT;
-            else next_state = START;
+            if (s_axis_config_tready) next_state <= CONFIG_FFT;
+            else next_state <= START;
         end
 
         CONFIG_FFT: begin                    // configure FFT
-            s_axis_config_tvalid     = 1'b1;
-            s_axis_data_tvalid       = 1'b0;
-            s_axis_data_tlast        = 1'b0;
-            samples_sent_counter_sel = ZERO;
-            //start_frame              = 1'b0;
-            //end_frame                = 1'b0;
-            send_frame               = 1'b0;
+            s_axis_config_tvalid     <= 1'b1;
+            s_axis_data_tvalid       <= 1'b0;
+            s_axis_data_tlast        <= 1'b0;
+            samples_sent_counter_sel <= ZERO;
+            //start_frame              <= 1'b0;
+            //end_frame                <= 1'b0;
+            send_frame               <= 1'b0;
             
-            next_state = WAIT;
+            next_state <= WAIT;
         end
 
         WAIT: begin                    // wait for enable
-            s_axis_config_tvalid     = 1'b0;
-            s_axis_data_tvalid       = 1'b0;
-            s_axis_data_tlast        = 1'b0;
-            samples_sent_counter_sel = ZERO;
+            s_axis_config_tvalid     <= 1'b0;
+            s_axis_data_tvalid       <= 1'b0;
+            s_axis_data_tlast        <= 1'b0;
+            samples_sent_counter_sel <= ZERO;
             //start_frame              = 1'b0;
             //end_frame                = 1'b0;
-            send_frame               = 1'b0;
+            send_frame               <= 1'b0;
 
-            if (enable) next_state = START_FRAME;
-            else next_state = WAIT;
+            if (enable) next_state <= START_FRAME;
+            else next_state <= WAIT;
         end
 
         START_FRAME: begin                    // signal to outside that we're sending a frame
-            s_axis_config_tvalid     = 1'b0;
-            s_axis_data_tvalid       = 1'b0;
-            s_axis_data_tlast        = 1'b0;
-            samples_sent_counter_sel = ZERO;
+            s_axis_config_tvalid     <= 1'b0;
+            s_axis_data_tvalid       <= 1'b0;
+            s_axis_data_tlast        <= 1'b0;
+            samples_sent_counter_sel <= ZERO;
             //start_frame              = 1'b1;
             //end_frame                = 1'b0;
-            send_frame               = 1'b1;
+            send_frame               <= 1'b1;
 
-            next_state = SENDING;
+            next_state <= SENDING;
         end
 
         SENDING: begin                    // send a frame
-            s_axis_config_tvalid     = 1'b0;
-            s_axis_data_tvalid       = 1'b1;
-            s_axis_data_tlast        = 1'b0;
-            samples_sent_counter_sel = COUNT;
+            s_axis_config_tvalid     <= 1'b0;
+            s_axis_data_tvalid       <= 1'b1;
+            s_axis_data_tlast        <= 1'b0;
+            samples_sent_counter_sel <= COUNT;
             //start_frame              = 1'b0;
             //end_frame                = 1'b0;
-            send_frame               = 1'b1;
+            send_frame               <= 1'b1;
 
-            if (samples_sent_count_reached) next_state = END_FRAME;
-            else next_state = SENDING;
+            if (samples_sent_count_reached) next_state <= END_FRAME;
+            else next_state <= SENDING;
         end
 
         END_FRAME: begin                    // send the last sample & assert end_frame
-            s_axis_config_tvalid     = 1'b0;
-            s_axis_data_tvalid       = 1'b1;
-            s_axis_data_tlast        = 1'b1;
-            samples_sent_counter_sel = ZERO;
+            s_axis_config_tvalid     <= 1'b0;
+            s_axis_data_tvalid       <= 1'b1;
+            s_axis_data_tlast        <= 1'b1;
+            samples_sent_counter_sel <= ZERO;
             //start_frame              = 1'b0;
             //end_frame                = 1'b1;
-            send_frame               = 1'b0;
+            send_frame               <= 1'b0;
 
-            next_state = WAIT;
+            next_state <= WAIT;
         end
 
         default: begin
-            s_axis_config_tvalid     = 1'b0;
-            s_axis_data_tvalid       = 1'b0;
-            s_axis_data_tlast        = 1'b0;
-            samples_sent_counter_sel = ZERO;
+            s_axis_config_tvalid     <= 1'b0;
+            s_axis_data_tvalid       <= 1'b0;
+            s_axis_data_tlast        <= 1'b0;
+            samples_sent_counter_sel <= ZERO;
             //start_frame              = 1'b0;
             //end_frame                = 1'b0;
-            send_frame               = 1'b0;
+            send_frame               <= 1'b0;
 
-            next_state = START;
+            next_state <= START;
         end
     endcase
 end
+
+LONG_SIG_CNT_CONTROLLER fourth_sample_cnt_ctrl_inst(.clk(clk),
+                                                    .reset_b(reset_b),
+                                                    .incr_signal(data_ready),
+                                                    .count_reached(fourth_sample_reached),
+                                                    .counter_sel(fourth_sample_counter_sel)
+);
+
+endmodule
+
+module LONG_SIG_CNT_CONTROLLER(     // General Counter driver that waits for a persistent signal (like data_ready) to go low
+                                    // Don't use this if you expect the signal to toggle every clock cycle
+    input clk,
+    input reset_b,
+    input incr_signal,  // signal whose going high should trigger an increment
+    input count_reached,
+    output reg [1:0] counter_sel
+);
+
+localparam [1:0] 
+    // counter_sel values
+    ZERO  = 2'b00, 
+    HOLD  = 2'b10,
+    COUNT = 2'b11,
+
+    // states
+    ZERO_STATE = 2'b00,   // zero the counter
+    HOLD_STATE = 2'b01,           // hold current value
+    COUNT_STATE = 2'b10,          // when incr_signal goes high, set counter_sel=COUNT for 1 clock cycle
+    HOLDANDWAIT = 2'b11;    // wait for incr_signal to go low again
+
+reg [1:0] current_state, next_state;
+
+always @ (posedge clk or negedge reset_b) begin
+    if (!reset_b) current_state = ZERO;
+    else current_state = next_state;
+end
+
+always @ (*) begin
+    case (current_state)
+        ZERO_STATE: begin
+            counter_sel <= ZERO;
+            next_state <= HOLD_STATE;
+        end
+
+        HOLD_STATE: begin
+            counter_sel <= HOLD;
+            if (incr_signal) next_state <= COUNT_STATE;
+            else next_state <= HOLD_STATE;
+        end
+
+        COUNT_STATE: begin
+            counter_sel <= COUNT;
+            if (incr_signal) next_state <= HOLDANDWAIT;
+            else next_state <= HOLD_STATE; // if incr_signal goes low skip directly to hold_state
+        end
+
+        HOLDANDWAIT: begin
+            counter_sel <= count_reached ? ZERO : HOLD;
+            if (!incr_signal) next_state <= HOLD_STATE;
+            else next_state <= HOLDANDWAIT;
+        end
+
+        default: begin
+            counter_sel <= ZERO;
+            next_state <= ZERO_STATE;
+        end
+    endcase
+end
+
 endmodule
--- src/TRIGGER_FFT.v
+++ src/TRIGGER_FFT.v
@@ -29,7 +29,7 @@ localparam [5:0] SCALE_SCHEDULE = 6'b101011;
 localparam FWD = 1'b1;
 
 // WIRES ------------------------------------------------------------------------------------------
-wire [1:0] fourth_sample_counter_sel;   // counter signals
+wire [1:0] fourth_sample_counter_sel;
 wire [1:0] samples_sent_counter_sel;
 wire fourth_sample_reached;
 wire samples_sent_count_reached;
@@ -55,8 +55,7 @@ reg [5:0] addra;
 wire [9:0] dina;
 
 // SPI data_ready counter -- when 4 reached, start the FFT
-assign fourth_sample_counter_sel = data_ready ? COUNT : HOLD;
- 
+
 GENERAL_COUNTER #(.COUNT_VAL(3), .COUNT_BIT_WIDTH(2)) fourth_sample_counter (
     .clk(clk),
     .reset_b(reset_b),
@@ -77,6 +76,8 @@ assign ok_to_start_frame = trigger_fft_enable & fourth_sample_reached;
 // State Machine
 FFT_CONTROLLER trigger_fft_controller(  .clk(clk),
                                         .reset_b(reset_b),
+                                        .data_ready(data_ready),
+                                        .fourth_sample_reached(fourth_sample_reached),
                                         .s_axis_config_tready(s_axis_config_tready),
                                         .enable(ok_to_start_frame),
                                         .samples_sent_count_reached(samples_sent_count_reached),
@@ -85,6 +86,7 @@ FFT_CONTROLLER trigger_fft_controller(  .clk(clk),
                                         .s_axis_data_tvalid(s_axis_data_tvalid),
                                         .s_axis_data_tlast(s_axis_data_tlast),
                                         .samples_sent_counter_sel(samples_sent_counter_sel),
+                                        .fourth_sample_counter_sel(fourth_sample_counter_sel),
                                         //.start_frame(start_frame),
                                         //.end_frame(end_frame)
                                         .send_frame(send_frame)
@@ -95,6 +97,18 @@ assign s_axis_config_tdata = {CONFIG_ZERO_PAD, SCALE_SCHEDULE, FWD};
 assign s_axis_data_tdata = {16'b0, 6'b0, fft_real_data_in}; // {imaginary data, zero padding, real data}
 assign m_axis_data_tready = 1'b1;
 assign fft_real_data_out = m_axis_data_tdata[9:0];
+
+// unused FFT wires
+wire m_axis_data_tlast;
+wire [7:0] m_axis_status_tdata;
+wire m_axis_status_tvalid;
+wire event_frame_started;
+wire event_tlast_unexpected;
+wire event_tlast_missing;
+wire event_fft_overflow;
+wire event_status_channel_halt;
+wire event_data_in_channel_halt;
+wire event_data_out_channel_halt;
 
 xfft_trigger trigger_fft (
   .aclk(clk),                                                // input wire aclk
@@ -113,14 +127,14 @@ xfft_trigger trigger_fft (
   .m_axis_data_tlast(m_axis_data_tlast),                      // output wire m_axis_data_tlast
   .m_axis_status_tdata(m_axis_status_tdata),                  // output wire [7 : 0] m_axis_status_tdata
   .m_axis_status_tvalid(m_axis_status_tvalid),                // output wire m_axis_status_tvalid
-  .m_axis_status_tready(m_axis_status_tready),                // input wire m_axis_status_tready
+  .m_axis_status_tready(1'b1),                                // input wire m_axis_status_tready
   .event_frame_started(event_frame_started),                  // output wire event_frame_started
   .event_tlast_unexpected(event_tlast_unexpected),            // output wire event_tlast_unexpected
   .event_tlast_missing(event_tlast_missing),                  // output wire event_tlast_missing
   .event_fft_overflow(event_fft_overflow),                    // output wire event_fft_overflow
   .event_status_channel_halt(event_status_channel_halt),      // output wire event_status_channel_halt
   .event_data_in_channel_halt(event_data_in_channel_halt),    // output wire event_data_in_channel_halt
-  .event_data_out_channel_halt(event_data_out_channel_halt)  // output wire event_data_out_channel_halt
+  .event_data_out_channel_halt(event_data_out_channel_halt)   // output wire event_data_out_channel_halt
 );
 
 // Write FFT output to RAM
@@ -145,10 +159,11 @@ dual_port_ram_10x64 fft_output_RAM (
   .enb(1'b1),      // input wire enb
   .web(1'b0),      // input wire [0 : 0] web
   .addrb(fft_output_RAM_addr),  // input wire [5 : 0] addrb
-  .dinb(dinb),    // input wire [9 : 0] dinb
+  .dinb(10'b0),    // input wire [9 : 0] dinb
   .doutb(fft_output_RAM_data)  // output wire [9 : 0] doutb
 );
 
-
+// unused RAM wires
+wire [9:0] douta;
 
 endmodule
