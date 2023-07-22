02d7fda1be97efe10e417281caeca532b92ad5a9
imjohns3 <imjohns3@ncsu.edu>
2023-07-22 12:55:30 -0400

    fixed testbenches

--- srcsim/TRIGGER_FFT_tb.v
+++ srcsim/TRIGGER_FFT_tb.v
@@ -1,5 +1,5 @@
 // TRIGGER FFT Testbench -- July 20, 2023
-// Simulates the entire top-level module with Chris' rewritten TRIGGER_FFT_v2 and RING_BUFFER modules
+// Simulates the entire top-level module with new TRIGGER_FFT and RING_BUFFER modules
 
 `timescale 1ns / 1ps
 
@@ -7,12 +7,7 @@
 module TRIGGER_FFT_tb();
 
 reg clk = 1'b0;
-reg spi_clk = 1'b0;
 reg reset_b = 1'b1;
-
-reg [3:0] frequency = 4'd18;
-reg [3:0] threshold = 4'b0;
-reg [1:0] offset = 2'b0;
 
 reg data_ready = 1'b0;
 reg [9:0] data_array [0:255];
@@ -24,15 +19,12 @@ wire fft_output_RAM_ready;
 wire RAM_overflow;
 wire [9:0] output_data;
 
-wire fft_data_ready;
-wire trigger;
-
 integer index = 0;
 event assert_data_ready;
 
 initial
-//$readmemh("/home/cpmori/Documents/AquaPack/SW8A-AcousticsFPGA/srcsim/TRIGGER_FFT_tb_input.data", data_array);
-$readmemh("C:/Users/ilena/Documents/apr-private/SW8A-AcousticsFPGA/srcsim/TRIGGER_FFT_tb_input.data", data_array);
+$readmemh("/home/cpmori/Documents/AquaPack/SW8A-AcousticsFPGA/srcsim/TRIGGER_FFT_tb_input.data", data_array);
+
 
 always begin // assert_data_ready
     @(assert_data_ready);
@@ -45,7 +37,6 @@ end
 always #1960 -> assert_data_ready;
 
 always #5 clk = ~clk;
-always #70 spi_clk = ~spi_clk;
 
 RING_BUFFER dut1(.clk(clk),
                 .reset_b(reset_b),
@@ -56,18 +47,15 @@ RING_BUFFER dut1(.clk(clk),
                 .Output_Data(output_data)
 );
 
-TRIGGER_FFT_v2 dut2(   .clk(clk),
-                .SPI_CLK(spi_clk),
+TRIGGER_FFT dut2(   .clk(clk),
                 .reset_b(reset_b),
+                .trigger_fft_enable(1'b1),
                 .data_ready(data_ready), // from SPI
-                .Input_Data(output_data), // from RAM --> FFT
-                .Offset(offset),
-                .Frequency(frequency),
-                .Threshold(threshold),
-
-                .Send_Frame(send_frame),
-                .FFT_Data_Ready(fft_data_ready),
-                .Trigger(trigger)
+                .fft_real_data_in(output_data), // from RAM --> FFT
+                .fft_output_RAM_addr(),
+                .send_frame(send_frame),
+                .fft_output_RAM_data(output_data),
+                .fft_output_RAM_ready(fft_output_RAM_ready)
 );
 
 
