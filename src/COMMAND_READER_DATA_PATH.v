391a262c31d788fc37e88cd39925f21275e2f9af
imjohns3 <imjohns3@ncsu.edu>
2023-09-27 19:02:14 -0400

    added unverified CDCs in cmd dp

--- src/COMMAND_READER_DATA_PATH.v
+++ src/COMMAND_READER_DATA_PATH.v
@@ -42,8 +42,6 @@ module COMMAND_READER_DATA_PATH(
         TRUE = 2'b10,
         FALSE = 2'b11;
     
-
-    
     reg [7:0] Next_Word_To_Send;
     
     assign Channel_sel = Command[2:0];
@@ -78,16 +76,36 @@ module COMMAND_READER_DATA_PATH(
         endcase
     end
     
+    // CDC handling for Timeout
+    wire timeout_int;
+    xpm_cdc_single #(
+        .DEST_SYNC_FF(4),   // DECIMAL; range: 2-10
+        .INIT_SYNC_FF(0),   // DECIMAL; 0=disable simulation init values, 1=enable simulation init values
+        .SIM_ASSERT_CHK(1), // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+        .SRC_INPUT_REG(1)   // DECIMAL; 0=do not register input, 1=register input
+    )
+    xpm_cdc_single_inst (
+        .dest_out(Timeout), // 1-bit output: src_in synchronized to the destination clock domain. This output is
+                               // registered.
+
+        .dest_clk(clk), // 1-bit input: Clock signal for the destination clock domain.
+        .src_clk(slow_clk),   // 1-bit input: optional; required when SRC_INPUT_REG = 1
+        .src_in(timeout_int)      // 1-bit input: Input signal to be synchronized to dest_clk domain.
+    );
+    
+    // CDC for timer_sel
+    integer i;
+    (* ASYNC_REG = "TRUE" *) reg [1:0] timer_sel_sync [0:3];
+    always @ (posedge slow_clk) begin
+        for (i=3; i>0; i=i-1) timer_sel_sync[i] <= timer_sel_sync[i-1];
+        timer_sel_sync[0] <= Timer_sel;
+    end
+    
     GENERAL_COUNTER  #(.COUNT_VAL(23040000), . COUNT_BIT_WIDTH(25)) Timer (
         .clk(slow_clk),
         .reset_b(reset_b),
-        .Count_sel(Timer_sel),
-        .Count_Reached(Timeout)
+        .Count_sel(timer_sel_sync[3]),
+        .Count_Reached(timeout_int) // timeout internal --> CDC
     );
-    
-    
-    
-    
-    
 
 endmodule
