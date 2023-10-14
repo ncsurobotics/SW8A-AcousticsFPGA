7820e5a8b68fc57638bc37af4c5253ec3dd55db5
imjohns3 <imjohns3@ncsu.edu>
2023-10-14 11:12:56 -0400

    add w2s in full_sys_tb

--- srcsim/Full_Sys_tb.v
+++ srcsim/Full_Sys_tb.v
@@ -171,7 +171,8 @@ initial begin
     $readmemh("C:/Users/ilena/Documents/apr-private/fpga/SW8A-AcousticsFPGA/srcsim/cc_datasets/trigger_rshift.data", y_data_array);
     $readmemh("C:/Users/ilena/Documents/apr-private/fpga/SW8A-AcousticsFPGA/srcsim/cc_datasets/trigger.data", x_data_array);
     fp_trig_out = $fopen("C:/Users/ilena/Documents/apr-private/fpga/SW8A-AcousticsFPGA/srcsim/trig_out.txt");
-    #100 drive_uart_tx(8'h41);
+    #100 drive_uart_tx(8'h41); // send max ch1
+    //#100 drive_uart_tx(8'hd0); // trigger detect
 
     //#276500 ok_to_write = 1'b1;
     //@ (negedge tb_trigger_fft_tlast) ok_to_write = 1'b0;
